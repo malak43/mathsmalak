3692674eee7f164cf7709aaff5e28963b726e290
malak43 <80445405+malak43@users.noreply.github.com>
2021-03-11 10:02:14 +0100

    Add files via upload

---
+++ fichier.js
@@ -0,0 +1,306 @@
+function eq1(){
+    var text,x ;
+    x=document.getElementById('eq1').value;
+    if (x=='non quadrilatère') 
+    { 
+        text=" bravo !! votre réponse est correcte.";
+        document.getElementById("v1").innerHTML = text;
+    } 
+    else
+       { text="réponse fausse";
+    document.getElementById("f1").innerHTML = text +", la solution juste est non quadrilatère.";
+       }
+    }
+
+    function eq2(){
+              var text,x ;
+              x=document.getElementById('eq2').value; 
+              if (x=='carré')
+               { text=" bravo !! votre réponse est correcte.";
+              document.getElementById("v2").innerHTML = text;}
+              else
+              { text="reponse fausse";
+              document.getElementById("f2").innerHTML = text + ", la solution juste est carré.";}
+              }
+
+
+      function eq3(){
+                     var text,x ;
+                     x=document.getElementById('eq3').value; 
+                     if (x=='non quadrilatère') 
+                     { text=" bravo !! votre réponse est correcte.";
+                     document.getElementById("v3").innerHTML = text;
+                     }
+                     else{ text="reponse fausse";
+                     document.getElementById("f3").innerHTML = text + ", la solution juste est non quadrilatère .";}
+                        }    
+
+
+         function eq4(){
+                            var text,x ;
+                            x=document.getElementById('eq4').value; 
+                            if (x=='rectangle') 
+                            { text=" bravo !! votre réponse est correcte.";
+                            document.getElementById("v4").innerHTML = text;
+                            }
+                            else{ text="reponse fausse";
+                            document.getElementById("f4").innerHTML = text + ", la solution juste rectangle.";}
+                           }
+
+            function eq5(){
+                               var x ;
+                               x=document.getElementById('eq5').value; 
+                               if (x=='340') 
+                               { text=" bravo !! votre réponse est correcte.";
+                              document.getElementById("v5").innerHTML = text;
+                               }
+                              else{ text="reponse fausse";
+                              document.getElementById("f5").innerHTML = text + ", la solution juste 340.";}
+                               }
+
+               function eq6(){
+                                    var x ;
+                                    x=document.getElementById('eq6').value; 
+                                    if (x=='600') 
+                                    { text=" bravo !! votre réponse est correcte.";
+                                   document.getElementById("v6").innerHTML = text;
+                                    }
+                                   else{ text="reponse fausse";
+                                   document.getElementById("f6").innerHTML = text + ", la solution juste 700.";}
+                                    }
+
+                  function eq7(){
+                                          var x ;
+                                          x=document.getElementById('eq7').value; 
+                                          if (x=='23') 
+                                          { text=" bravo !! votre réponse est correcte.";
+                                         document.getElementById("v7").innerHTML = text;
+                                          }
+                                         else{ text="reponse fausse";
+                                         document.getElementById("f7").innerHTML = text + ", la solution juste 23.";}
+                                          }
+
+                     function eq8(){
+                                           var x ;
+                                           x=document.getElementById('eq8').value; 
+                                          if (x=='6 200') 
+                                          { text=" bravo !! votre réponse est correcte.";
+                                           document.getElementById("v8").innerHTML = text;
+                                           }
+                                           else{ text="reponse fausse";
+                                           document.getElementById("f8").innerHTML = text + ", la solution juste 6 200.";}
+                                            }
+
+                        function el1(){
+                                              var x ;
+                                              x=document.getElementById('el1').value; 
+                                             if (x=='10') 
+                                             { text=" bravo !! votre réponse est correcte.";
+                                              document.getElementById("vl1").innerHTML = text;
+                                              }
+                                              else{ text="reponse fausse";
+                                              document.getElementById("fl1").innerHTML = text + ", la solution juste 10.";}
+                                               }
+                           function el2(){
+
+                                             var x ;
+                                              x=document.getElementById('el2').value; 
+                                             if (x=='20') 
+                                             { text=" bravo !! votre réponse est correcte.";
+                                              document.getElementById("vl2").innerHTML = text;
+                                              }
+                                              else{ text="reponse fausse";
+                                              document.getElementById("fl2").innerHTML = text + ", la solution juste 20.";}
+                                               }
+                           function el3(){
+
+                              var x ;
+                               x=document.getElementById('el3').value; 
+                              if (x=='34') 
+                              { text=" bravo !! votre réponse est correcte.";
+                               document.getElementById("vl3").innerHTML = text;
+                               }
+                               else{ text="reponse fausse";
+                               document.getElementById("fl3").innerHTML = text + ", la solution juste 34.";}
+                                }
+            function el4(){
+
+               var x ;
+                   x=document.getElementById('el3').value; 
+                  if (x=='1') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vl4").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fl4").innerHTML = text + ", la solution juste 1.";}
+                    }
+
+function el5(){
+
+                  var x ;
+                   x=document.getElementById('el5').value; 
+                  if (x=='7') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vl5").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fl5").innerHTML = text + ", la solution juste 7.";}
+                    }
+
+function el6(){
+
+                   var x ;
+                   x=document.getElementById('el6').value; 
+                  if (x=='41') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vl6").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fl6").innerHTML = text + ", la solution juste 41.";}
+                    }
+
+                                            
+function el7(){
+
+                   var x ;
+                   x=document.getElementById('el7').value; 
+                  if (x=='18') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vl7").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fl7").innerHTML = text + ", la solution juste 18.";}
+                    }
+
+function el8(){
+
+                   var x ;
+                   x=document.getElementById('el8').value; 
+                  if (x=='36') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vl8").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fl8").innerHTML = text + ", la solution juste 36.";}
+                    } 
+                           
+function el9(){
+
+                   var x ;
+                   x=document.getElementById('el9').value; 
+                  if (x=='30') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vl9").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fl9").innerHTML = text + ", la solution juste 30.";}
+                    } 
+                                             
+function em1(){
+
+                  var x ;
+                   x=document.getElementById('em1').value; 
+                  if (x=='4') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vm1").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fm1").innerHTML = text + ", la solution juste 4.";}
+                    } 
+
+function em2(){
+
+                  var x ;
+                   x=document.getElementById('em2').value; 
+                  if (x=='5') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vm2").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fm2").innerHTML = text + ", la solution juste 5.";}
+                    }       
+
+function em3(){
+
+                   var x ;
+                   x=document.getElementById('em3').value; 
+                  if (x=='8') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vm3").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fm3").innerHTML = text + ", la solution juste 8.";}
+                    }       
+
+function em4(){
+
+                   var x ;
+                   x=document.getElementById('em4').value; 
+                  if (x=='les heures') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vm4").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fm4").innerHTML = text + ", la solution juste les heures.";}
+                    }       
+
+function em5(){
+
+                   var x ;
+                   x=document.getElementById('em5').value; 
+                  if (x=='les minutes') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vm5").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fm5").innerHTML = text + ", la solution juste les minutes.";}
+                    }          
+
+function em6(){
+
+                   var x ;
+                   x=document.getElementById('em6').value; 
+                  if (x=='les secondes') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vm6").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fm6").innerHTML = text + ", la solution juste les secondes.";}
+                    }          
+
+function em7(){
+
+                   var x ;
+                   x=document.getElementById('em7').value; 
+                  if (x=='il est midi et demi') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vm7").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fm7").innerHTML = text + ", la solution juste il est midi et demi.";}
+                    }          
+
+
+function em8(){
+
+                   var x ;
+                   x=document.getElementById('em8').value; 
+                  if (x=='il est 10 heures et quart') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vm8").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fm8").innerHTML = text + ", la solution juste il est 10 heures et quart.";}
+                    }          
+
+function em9(){
+
+                   var x ;
+                   x=document.getElementById('em9').value; 
+                  if (x=='il est 4 heures moins quart') 
+                  { text=" bravo !! votre réponse est correcte.";
+                   document.getElementById("vm9").innerHTML = text;
+                   }
+                   else{ text="reponse fausse";
+                   document.getElementById("fm9").innerHTML = text + ", la solution juste il est 4 heures moins quart.";}
+                    }          
